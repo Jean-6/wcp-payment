89dc4ab1789724ed6e13bd3de6a1e9eeca01718e
Jean <oka.jeanjaures@gmail.com>
2025-04-01 17:48:32 +0200

    redefine bootstrapApp with promise handling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideNgxStripe} from 'ngx-stripe';
+import {provideHttpClient} from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideNgxStripe('pk_live_51Lgb1fFC4qwBoSWo85KiVZ7uPM6FRIATADdNPKt06wN1F2Qp5KW5KfeJ0lBeTeuG3Gom1Au2aHgNMqpu3OKe1v2z00B1olQlsX')
+  ],
+}).then(() => console.log('Application started with success') )
+  .catch(err => console.log('Error during launching ',err));
