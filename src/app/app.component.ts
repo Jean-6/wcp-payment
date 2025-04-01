a1c1e91ed2a2a7dc524cac944efba5a7b8d845b4
Jean <oka.jeanjaures@gmail.com>
2025-04-02 00:25:11 +0200

    declare stripe component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {StripePaymentComponent} from './features/stripe-payment/stripe-payment.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule,StripePaymentComponent],
+  //templateUrl: './app.component.html',
+  template: `<app-stripe-payment></app-stripe-payment>`,
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {StripePaymentComponent} from './features/stripe-payment/stripe-payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  { path: 'stripe-payment', component: StripePaymentComponent },
+  { path: '**', redirectTo: 'stripe-payment'},
+
+];
