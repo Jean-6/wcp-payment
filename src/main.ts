599c6d8aa33bb659d80e70a67cc444b965e3891c
Jean <oka.jeanjaures@gmail.com>
2025-04-03 00:47:35 +0200

    installing bootstrap and angular animations

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideNgxStripe} from 'ngx-stripe';
 import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAnimations(),
     provideHttpClient(),
     provideNgxStripe('pk_live_51Lgb1fFC4qwBoSWo85KiVZ7uPM6FRIATADdNPKt06wN1F2Qp5KW5KfeJ0lBeTeuG3Gom1Au2aHgNMqpu3OKe1v2z00B1olQlsX')
   ],
